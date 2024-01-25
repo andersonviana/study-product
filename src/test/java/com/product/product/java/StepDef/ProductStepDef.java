0288a80cffa1a919cad17ba2ba0a83a07e3f6690
Anderson Silva Viana <anviana@uolinc.com>
2024-01-24 21:45:48 -0300

    Exemplo de configuracao e implementaçao do cucumberjava8 com lambda e config de ex de stream

---
+++ src/test/java/com/product/product/java/StepDef/ProductStepDef.java
@@ -0,0 +1,45 @@
+package com.product.product.java.StepDef;
+
+
+import com.product.product.model.Produto;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java8.Pt;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class ProductStepDef implements Pt {
+    public ProductStepDef() {
+
+        Dado("^que o cliente adiciona um produto de ids:$", (DataTable dataTable) -> {
+            List<List<String>> produtoList = dataTable.asLists(String.class);
+            System.out.println(produtoList.size());
+
+            Produto pd = new Produto();
+
+            produtoList.stream()
+                    .skip(1)
+                    .filter(e -> e.contains("caneta"))
+                    .forEach(e -> pd.setNome(e.get(1)));
+
+            List<List<String>> caneta = produtoList.stream()
+                    .skip(1)
+                    .filter(e -> e.contains("caneta"))
+                    .toList();
+
+
+                caneta.stream().forEach(e -> System.out.println(e));
+            System.out.println(caneta.size());
+
+            for (List<String> eas : produtoList) {
+                System.out.println("\n " + eas.toString());
+            }
+        });
+        Quando("^o cliente envia a solicitacao de cadastro$", () -> {System.out.println("Envia a solicitacao de cadastro");});
+        Entao("^o cliente recebe o response com todos os produtos cadastrados$", () -> {System.out.println("O produto é cadastrado");});
+        E("^o id enviado foi ignorado$", () -> {System.out.println("Envia  o id enviado é ignorado");});
+
+    }
+}
+
