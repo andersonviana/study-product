b3dbdeb013449bb3669a090b9193fdf6b07da946
Anderson Silva Viana <anviana@uolinc.com>
2024-01-26 10:42:19 -0300

    STD-003 Adição do java doc

--- src/main/java/com/product/product/controller/ProdutoController.java
+++ src/main/java/com/product/product/controller/ProdutoController.java
@@ -1,8 +1,12 @@
 package com.product.product.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +33,17 @@ public class ProdutoController {
         return produtoService.obterTodos();
     }
 
+
+    /**
+     * Metodo para obter por id no service
+     * @param id
+     * @return um produto repository por id
+     */
+    @GetMapping("/{id}")
+    public Optional<Produto> obterPorid(@PathVariable Integer id){
+        return produtoService.obterPorid(id);
+    }
+
     /**
      * Endpoint para adicionar um produto.
      * @param produto
@@ -38,5 +53,28 @@ public class ProdutoController {
     public Produto adicionarProduto(@RequestBody Produto produto){
         return produtoService.adicionar(produto);
     }
+
+    /**
+     * Metodo para deletar um produto
+     * @param id
+     * @return mensgem de produto deletado
+     */
+    @DeleteMapping("/{id}")
+    public String deletar(@PathVariable Integer id){
+        produtoService.deletar(id);
+        return "Produto com id: " + id + " foi deletado com sucesso";
+    }
+
+    /**
+     * Endpoint para adicionar um produto.
+     * @param produto
+     * @return Produto criado.
+     */
+    @PutMapping("{id}/atualizar")
+    public Produto atualizarProduto(@RequestBody Produto produto, @PathVariable Integer id){
+        return produtoService.atualizar(id, produto);
+    }
+
+
 
 }
