1007953f834049c8ba11d6ca0cbe6e5768996cbf
Anderson Silva Viana <anviana@uolinc.com>
2024-01-26 10:40:55 -0300

    STD-003 Configuracao do cucumber com lambda.

--- src/test/java/com/product/product/java/StepDef/ProductStepDef.java
+++ src/test/java/com/product/product/java/StepDef/ProductStepDef.java
@@ -14,12 +14,12 @@ public class ProductStepDef implements Pt {
             List<List<String>> produtoList = dataTable.asLists(String.class);
             System.out.println(produtoList.size());
 
-            Produto pd = new Produto();
-
             produtoList.stream()
                     .skip(1)
                     .filter(e -> e.contains("caneta"))
-                    .forEach(e -> pd.setNome(e.get(1)));
+                    .findFirst();
+
+            System.out.println(produtoList.toString());
 
             List<List<String>> caneta = produtoList.stream()
                     .skip(1)
@@ -27,16 +27,22 @@ public class ProductStepDef implements Pt {
                     .toList();
 
 
-                caneta.stream().forEach(e -> System.out.println(e));
+            caneta.stream().forEach(e -> System.out.println(e));
             System.out.println(caneta.size());
 
             for (List<String> eas : produtoList) {
                 System.out.println("\n " + eas.toString());
             }
         });
-        Quando("^o cliente envia a solicitacao de cadastro$", () -> {System.out.println("Envia a solicitacao de cadastro");});
-        Entao("^o cliente recebe o response com todos os produtos cadastrados$", () -> {System.out.println("O produto é cadastrado");});
-        E("^o id enviado foi ignorado$", () -> {System.out.println("Envia  o id enviado é ignorado");});
+        Quando("^o cliente envia a solicitacao de cadastro$", () -> {
+            System.out.println("Envia a solicitacao de cadastro");
+        });
+        Entao("^o cliente recebe o response com todos os produtos cadastrados$", () -> {
+            System.out.println("O produto é cadastrado");
+        });
+        E("^o id enviado foi ignorado$", () -> {
+            System.out.println("Envia  o id enviado é ignorado");
+        });
 
     }
 }
